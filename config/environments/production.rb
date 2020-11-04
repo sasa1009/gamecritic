b7caeacc039f67163a18e7b0d528cd69520f9a1b
sasa1009 <conventional1009@gmail.com>
2020-11-04 00:02:37 +0000

    Add more credentials to the credentials.yml.enc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: 'gamecritic12345@gmail.com',
-    password: 'campus6300',
+    address: Rails.application.credentials.dig(:mail, :address),
+    domain: Rails.application.credentials.dig(:mail, :domain),
+    port: Rails.application.credentials.dig(:mail, :port),
+    user_name: Rails.application.credentials.dig(:mail, :user_name),
+    password: Rails.application.credentials.dig(:mail, :password),
     authentication: 'plain',
     enable_starttls_auto: true
   }
